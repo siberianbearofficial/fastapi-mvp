a8a38da18037d02249a704bed9ce0025ffc139da
Aleksei Orlov <61984103+siberianbearofficial@users.noreply.github.com>
2025-06-28 14:52:03 +0300

    S3 (#4)

--- fastapi_mvp/settings/settings.py
+++ fastapi_mvp/settings/settings.py
@@ -1,6 +1,8 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(from_attributes=True)
+
     def __hash__(self) -> int:
         return hash(self.model_dump_json())
---
+++ fastapi_mvp/storage/s3_storage/settings.py
@@ -0,0 +1,21 @@
+from pydantic_settings import SettingsConfigDict
+
+from fastapi_mvp.settings import Settings
+
+
+class S3Settings(Settings):
+    """
+    Settings that are used to configure S3 Storage.
+    You can provide values explicitly or via env vars prefixed with 'S3__'.
+    """
+
+    model_config = SettingsConfigDict(
+        env_prefix="S3__",
+    )
+
+    bucket: str
+    host: str
+    protocol: str
+    region: str
+    aws_access_key_id: str
+    aws_secret_access_key: str
